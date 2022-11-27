127c41949649bb760bec4bc752cfa345ea45f352
MatheusDev20 <matheusdev20@gmail.com>
2022-11-27 14:13:42 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HelloWorld from "./components/CounterComponent.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header>
+    <HelloWorld msg="You did it!" />
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
