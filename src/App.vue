8e43c5d6665585f0cbbb9f8d0018c6df1776fc95
MatheusDev20 <matheusdev20@gmail.com>
2022-11-30 21:21:55 -0300

    feat: starting music list project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
-import CounterComponent from "./components/CounterComponent.vue";
+import MusicInput from "./components/MusicInput/MusicInput.vue";
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Lets</h1>
-  <main>
-    <CounterComponent />
-  </main>
+  <div class="bg-gray-900 flex flex-col h-screen w-screen items-center gap-12">
+    <h1 class="mt-10 font-bold text-white text-4xl">My Music list</h1>
+
+    <main class="flex flex-col gap-12">
+      <!-- Main Input -->
+      <MusicInput />
+      <div>
+        <h3>Engenheiros do Hawai Piano Bar</h3>
+      </div>
+    </main>
+  </div>
 </template>
---
+++ src/components/MusicInput/MusicInput.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="flex flex-row gap-8">
+    <input />
+    <div class="flex flex-row">
+      <img src="assets/img/plus-icons.png" />
+    </div>
+    <h3 class="text-white">Adicionar nova música</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang=""></style>
